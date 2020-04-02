529b7c08615146cf1102c4f409b002138ab3fdc6
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-02 14:23:48 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Datasaur - Evolved
+
+Fork it, clone it, run `npm install`, run `npm test`, and practice your `.filter`, `.map`, and `.sort`.
+
+For a version with tests for helper functions--meaning you'll have to make separate helper functions, but you can be sure they work before you `.map` and `.filter` and `.sort`--check out [the unevolved version of this challenge](https://github.com/abbreviatedman/datasaur).
---
+++ main.js
@@ -0,0 +1,84 @@
+/***********************
+ * ITERATION FUNCTIONS *
+ **********************/
+
+
+
+/*********************************
+ * TEST SETUP CODE - DON'T TOUCH!*
+ ********************************/
+
+  if (typeof makeDino === 'undefined') {
+    makeDino = undefined
+  }
+
+  if (typeof singularizeDinos === 'undefined') {
+    singularizeDinos = undefined
+  }
+
+  if (typeof truncateDinos === 'undefined') {
+    truncateDinos = undefined
+  }
+
+  if (typeof makeAllExtinct === 'undefined') {
+    makeAllExtinct = undefined
+  }
+
+  if (typeof carnivoresOnly === 'undefined') {
+    carnivoresOnly = undefined
+  }
+
+  if (typeof herbivoresOnly === 'undefined') {
+    herbivoresOnly = undefined
+  }
+
+  if (typeof extinctOnly === 'undefined') {
+    extinctOnly = undefined
+  }
+
+  if (typeof notExtinct === 'undefined') {
+    notExtinct = undefined
+  }
+
+  if (typeof triassicOnly === 'undefined') {
+    triassicOnly = undefined
+  }
+
+  if (typeof notTriassic === 'undefined') {
+    notTriassic = undefined
+  }
+
+  if (typeof bySpecies === 'undefined') {
+    bySpecies = undefined
+  }
+
+  if (typeof byExtinctLast === 'undefined') {
+    byExtinctLast = undefined
+  }
+
+  if (typeof byCarnivoresFirst === 'undefined') {
+    byCarnivoresFirst = undefined
+  }
+
+  if (typeof byPeriod === 'undefined') {
+    byPeriod = undefined
+  }
+
+
+
+module.exports = {
+  makeDino,
+  singularizeDinos,
+  truncateDinos,
+  makeAllExtinct,
+  carnivoresOnly,
+  herbivoresOnly,
+  extinctOnly,
+  notExtinct,
+  triassicOnly,
+  notTriassic,
+  bySpecies,
+  byExtinctLast,
+  byCarnivoresFirst,
+  byPeriod,
+}
